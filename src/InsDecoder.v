e67aec20ce3715052f90d280a6a02dde30a29c77
ClownRS <136730884+ClownRS@users.noreply.github.com>
2025-04-17 21:43:37 +0800

    完成了初版译码器的verilog。

--- src/InsDecoder.v
+++ src/InsDecoder.v
@@ -2,7 +2,7 @@
 
 //宏定义清零操作，默认情况下所有指令输出均为0
 `define CLEAR_ALL_OUTPINTS  \
-    mechine_op = 8'b0;  \
+    machine_op = 8'b0;  \
     csr_op = 6'b0;  \
     jmp_op = 9'b0;  \
     alu_op = 19'b0; \
@@ -20,7 +20,7 @@ module InsDecoder(
     output wire [6:0] imm_7,
     output wire [19:0] imm_20,
     output wire [11:0] imm_12,
-    output reg [7:0] mechine_op,
+    output reg [7:0] machine_op,
     output reg [5:0] csr_op,
     output reg [8:0] jmp_op,
     output reg [18:0] alu_op,
@@ -43,20 +43,20 @@ module InsDecoder(
 
     function [18:0] get_alu_op_i(input [2:0] funct3, input [6:0] funct7);
         case (funct3) 
-            3'b000: get_alu_op_i = 19'b0000000000000000001;
-            3'b010: get_alu_op_i = 19'b0000000000000000010;
-            3'b011: get_alu_op_i = 19'b0000000000000000100;
-            3'b100: get_alu_op_i = 19'b0000000000000001000;
-            3'b110: get_alu_op_i = 19'b0000000000000010000;
-            3'b111: get_alu_op_i = 19'b0000000000000100000;
+            3'b000: get_alu_op_i = 19'b0000000000000000001; //addi
+            3'b010: get_alu_op_i = 19'b0000000000000000010; //slti
+            3'b011: get_alu_op_i = 19'b0000000000000000100; //sltiu
+            3'b100: get_alu_op_i = 19'b0000000000000001000; //xori
+            3'b110: get_alu_op_i = 19'b0000000000000010000; //ori
+            3'b111: get_alu_op_i = 19'b0000000000000100000; //andi
             //funct7
             3'b001: begin
-              if (funct7 == 7'b0) get_alu_op_i = 19'b0000000000001000000;
+              if (funct7 == 7'b0) get_alu_op_i = 19'b0000000000001000000;   //slli
             end
             3'b101: begin
               case (funct7)
-                7'b0: get_alu_op_i = 19'b0000000000010000000;
-                7'b0100000: get_alu_op_i = 19'b0000000000100000000;
+                7'b0: get_alu_op_i = 19'b0000000000010000000;   //srli
+                7'b0100000: get_alu_op_i = 19'b0000000000100000000; //srai
               endcase
             end
             default: begin
@@ -70,25 +70,92 @@ module InsDecoder(
         case (funct3)
             3'b000: begin
                 case (funct7)
-                    7'b0: get_alu_op_r = 19'b0000000001000000000;
-                    7'b0100000: get_alu_op_r = 19'b0000000010000000000;
+                    7'b0: get_alu_op_r = 19'b0000000001000000000;   //add
+                    7'b0100000: get_alu_op_r = 19'b0000000010000000000; //sub
                 endcase
             end
-            3'b001: if (funct7 == 7'b0) get_alu_op_r = 19'b0000000100000000000;
-            3'b010: if (funct7 == 7'b0) get_alu_op_r = 19'b0000001000000000000;
-            3'b011: if (funct7 == 7'b0) get_alu_op_r = 19'b0000010000000000000;
-            3'b100: if (funct7 == 7'b0) get_alu_op_r = 19'b0000100000000000000;
+            3'b001: if (funct7 == 7'b0) get_alu_op_r = 19'b0000000100000000000; //sll
+            3'b010: if (funct7 == 7'b0) get_alu_op_r = 19'b0000001000000000000; //slt
+            3'b011: if (funct7 == 7'b0) get_alu_op_r = 19'b0000010000000000000; //sltu
+            3'b100: if (funct7 == 7'b0) get_alu_op_r = 19'b0000100000000000000; //xor
             3'b101: begin
                 case (funct7) 
-                    7'b0: get_alu_op_r = 19'b0001000000000000000;
-                    7'b0100000: get_alu_op_r = 19'b0010000000000000000;
+                    7'b0: get_alu_op_r = 19'b0001000000000000000;   //srl
+                    7'b0100000: get_alu_op_r = 19'b0010000000000000000; //sra
                 endcase
             end
-            3'b110: if (funct7 == 7'b0) get_alu_op_r = 19'b0100000000000000000;
-            3'b111: if (funct7 == 7'b0) get_alu_op_r = 19'b1000000000000000000;
+            3'b110: if (funct7 == 7'b0) get_alu_op_r = 19'b0100000000000000000; //or
+            3'b111: if (funct7 == 7'b0) get_alu_op_r = 19'b1000000000000000000; //and
             default: begin
                 Invalid_Instruction = 32'd2;
                 get_alu_op_r = 19'b0;
+            end
+        endcase
+    endfunction
+
+    function [5:0] get_csr_op(input [2:0] funct3);
+        case (funct3)
+            3'b001: get_csr_op = 6'b000001; //csrrw
+            3'b010: get_csr_op = 6'b000010; //csrrs
+            3'b011: get_csr_op = 6'b000100; //csrrc
+            3'b101: get_csr_op = 6'b001000; //csrrwi
+            3'b110: get_csr_op = 6'b010000; //cssrrsi
+            3'b111: get_csr_op = 6'b100000; //csrrci
+            default: begin
+                Invalid_Instruction = 32'd2;
+                get_csr_op = 6'b0;
+            end
+        endcase
+    endfunction
+
+    function [8:0] get_jmp_op(input [2:0] funct3);
+        case (funct3)
+            3'b000: get_jmp_op = 9'b000000100;  //beq
+            3'b001: get_jmp_op = 9'b000001000;  //bne
+            3'b100: get_jmp_op = 9'b000010000;  //blt
+            3'b101: get_jmp_op = 9'b000100000;  //bge
+            3'b110: get_jmp_op = 9'b001000000;  //bltu
+            3'b111: get_jmp_op = 9'b010000000;  //bgeu
+            default: begin
+                get_jmp_op = 9'b0;
+                Invalid_Instruction = 32'd2;
+            end 
+        endcase
+    endfunction
+
+    function [8:0] get_mem_load_op(input [2:0] funct3);
+        case (funct3)
+            3'b000: get_mem_load_op = 9'b000000010;  //lb
+            3'b001: get_mem_load_op = 9'b000000100;  //lh
+            3'b010: get_mem_load_op = 9'b000001000;  //lw
+            3'b100: get_mem_load_op = 9'b000010000;  //lbu
+            3'b111: get_mem_load_op = 9'b000100000;  //lhu
+            default: begin
+                get_mem_load_op = 9'b0;
+                Invalid_Instruction = 32'd2;
+            end
+        endcase
+    endfunction
+
+    function [8:0] get_mem_store_op(input [2:0] funct3);
+        case (funct3)
+            3'b000: get_mem_store_op = 9'b001000000;  //sb
+            3'b001: get_mem_store_op = 9'b010000000;  //sh
+            3'b010: get_mem_store_op = 9'b010000000;  //sw
+            default: begin
+                get_mem_store_op = 9'b0;
+                Invalid_Instruction = 32'd2;
+            end
+        endcase
+    endfunction
+
+    function [7:0] get_machine_op(input [31:0] Instruction_Code);
+        case (Instruction_Code)
+            32'h100073: get_machine_op = 8'b00000001; //ebreak
+            32'h73: get_machine_op = 8'b00000010; //ecall
+            default: begin
+                get_machine_op = 8'b0;
+                Invalid_Instruction = 32'd2;
             end
         endcase
     endfunction
@@ -101,10 +168,11 @@ module InsDecoder(
         end
         else begin
             case (opcode)
-                //alu_op
+                //alu_op_i
                 7'b0010011: begin
                     alu_op = get_alu_op_i(funct3, funct7);
                 end
+                //alu_op_r
                 7'b0110011: begin
                     alu_op = get_alu_op_r(funct3, funct7);
                 end
@@ -112,12 +180,45 @@ module InsDecoder(
                 7'b0011111: begin
                     cust_op = 1'b1;
                 end
+                //csr_op
+                7'b1110011: begin
+                    if (funct3 == 3'b000) begin
+                        machine_op = get_machine_op(Instruction_Code);
+                    end
+                    else begin
+                        csr_op = get_csr_op(funct3);
+                    end    
+                end
+                //jmp_op
+                7'b1101111: begin
+                    jmp_op = 9'b000000001;  //jal
+                end
+                7'b1100111: begin
+                    jmp_op = 9'b000000001;  //jalr
+                end
+                7'b1100011: begin
+                    jmp_op = get_jmp_op(funct3);
+                end
+                //auipc
+                7'b0010111: begin
+                    jmp_op = 9'b100000000;
+                end
+                //mem_op
+                7'b0110111: begin
+                    mem_op = 9'b000000001;  //lui
+                end
+                7'b0000011: begin
+                    mem_op = get_mem_load_op(funct3);
+                end
+                7'b0100011: begin
+                    mem_op = get_mem_store_op(funct3);
+                end
             endcase
         end
-      end
-      else begin
-        `CLEAR_ALL_OUTPINTS;
-      end
     end
+    else begin
+        `CLEAR_ALL_OUTPINTS;
+    end
+end
 
 endmodule
